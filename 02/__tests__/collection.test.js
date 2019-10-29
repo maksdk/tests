f5014be78530da0e3809eafba42fa9de3c84a98a
maksdk <havryshchenko@gmail.com>
2019-10-29 22:19:56 +0200

    task 2

---
+++ 02/__tests__/collection.test.js
@@ -0,0 +1,9 @@
+import getFunction from '../functions';
+
+const get = getFunction();
+
+// BEGIN (write your solution here)
+if (get({ key: `value` }, `key`) !== `value`) throw new Error(`Error`);
+if (get({}, `key`, `defaultValue`) !== `defaultValue`) throw new Error(`Error`);
+if (get({ key: `value` }, `key`, `defaultValue`) !== `value`) throw new Error(`Error`);
+// END
